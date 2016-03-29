a5ae14f39fae80ce1d4db35854d66531c2106253
Devon Meunier <devon.meunier@shopify.com>
2016-03-29 16:36:14 -0400

    fish: add completion for rails generators

--- .config/fish/completions/bundle.fish
+++ .config/fish/completions/bundle.fish
@@ -13,9 +13,20 @@ function __fish_rake_tasks
     cat "$cache_file"
 end
 
-function __fish_bundle_exec_cmd
-    commandline -pc | egrep "^bundle\s+exec\s+$argv\s+\$"
-    return $status
+function __fish_bundle_subcommands
+    set -l cmd (commandline -poc)
+    set -e cmd[1]
+    if test (count $argv) != (count $cmd)
+        return 1
+    end
+    while test -n "$cmd"
+        if test $cmd[1] != $argv[1]
+            return 1
+        end
+        set -e cmd[1]
+        set -e argv[1]
+    end
+    return 0
 end
 
 function __fish_bundle_binstubs
@@ -34,6 +45,27 @@ function __fish_bundle_binstubs
     ls "$cache_dir"
 end
 
+function __fish_rails_generators
+    mkdir -p $HOME/.cache/rails_generators/completions
+    mkdir -p $HOME/.cache/rails_generators/fetch_head
 
-complete -c bundle -n '__fish_bundle_exec_cmd rake' -x -a '(__fish_rake_tasks)'
-complete -c bundle -n '__fish_seen_subcommand_from exec' -x -a '(__fish_bundle_binstubs)'
+    set -l pwd_hash (pwd | md5)
+    set -l cache_file "$HOME/.cache/rails_generators/completions/$pwd_hash"
+    set -l fetch_head "$HOME/.cache/rails_generators/fetch_head/$pwd_hash"
+
+    if begin not test -f $fetch_head; or test (cat .git/FETCH_HEAD | md5) != (cat $fetch_head); end
+        bundle exec rails generate --help | egrep '^\s+\w' | awk '{ print $1 }' > "$cache_file"
+        cat .git/FETCH_HEAD | md5 > $fetch_head
+    end
+    cat "$cache_file"
+end
+
+
+complete -c bundle -n '__fish_bundle_subcommands exec rake' -x -a '(__fish_rake_tasks)'
+complete -c bundle -n '__fish_bundle_subcommands exec' -x -a '(__fish_bundle_binstubs)'
+complete -c bundle -n '__fish_bundle_subcommands exec rails' -x -a 'generate' -d 'Generate new code'
+complete -c bundle -n '__fish_bundle_subcommands exec rails' -x -a 'console' -d 'Start the Rails console'
+complete -c bundle -n '__fish_bundle_subcommands exec rails' -x -a 'server' -d 'Start the Rails server'
+complete -c bundle -n '__fish_bundle_subcommands exec rails' -x -a 'server' -d 'Start the Rails server'
+complete -c bundle -n '__fish_bundle_subcommands exec rails' -x -a 'dbconsole' -d 'Start a console for the database specified in config/database.yml'
+complete -c bundle -n '__fish_bundle_subcommands exec rails generate' -x -a '(__fish_rails_generators)'
