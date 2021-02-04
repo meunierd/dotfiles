a73da24d22284edac43e3d5724530248f70fdf82
Devon Meunier <devon.meunier@shopify.com>
2021-02-04 09:39:59 -0500

    Install vim plugins

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,3 +3,5 @@ sudo apt-get install -y nodejs ripgrep neovim
 cp -r ~/dotfiles/.config/nvim ~/.config
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
 		https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+nvim +PluginInstall +qall
