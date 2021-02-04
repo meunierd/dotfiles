fef868706713910ea4bff0cd63c58a89a2ef0d0f
Devon Meunier <devon.meunier@shopify.com>
2021-02-04 09:52:37 -0500

    oops

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,4 +4,4 @@ cp -r ~/dotfiles/.config/nvim ~/.config
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
 		https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-nvim +PluginInstall +qall
+nvim +PlugInstall +qall
