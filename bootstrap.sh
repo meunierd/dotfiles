b53354de9566b254a2c2684a12c27ddeeb7333a1
Devon Meunier <devon.meunier@shopify.com>
2021-02-04 21:04:00 -0500

    Fix neovim bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,9 @@
-sudo apt-get install -y nodejs ripgrep neovim
+sudo apt-get install -y nodejs ripgrep neovim fzf
 
 cp -r ~/dotfiles/.config/nvim ~/.config
 cp ~/dotfiles/.tmux.conf ~
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
 		https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+nvim --headless +PlugInstall +qa
