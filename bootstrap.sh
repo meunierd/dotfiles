86855f364f6de2b93745444aa2e9815c502f3273
Devon Meunier <devon.meunier@shopify.com>
2021-02-04 19:46:38 -0500

    Add tmux

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 sudo apt-get install -y nodejs ripgrep neovim
 
 cp -r ~/dotfiles/.config/nvim ~/.config
+cp ~/dotfiles/.tmux.conf ~
+
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
 		https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
-nvim +PlugInstall +qall
