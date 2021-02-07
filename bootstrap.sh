3e18d89895d17070790899208099badc4fb9fc79
Devon Meunier <devon.meunier@shopify.com>
2021-02-06 20:59:46 -0500

    Fix auto attach

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,8 @@
 sudo apt-get install -y nodejs ripgrep neovim fzf
 
-ln -s ~/dotfiles/.config/nvim ~/.config
-ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -f -s ~/dotfiles/.config/nvim ~/.config
+ln -f -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -f -s ~/dotfiles/.zshrc ~/.zshrc
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
 		https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
