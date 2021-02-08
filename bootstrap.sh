d2bd4287a3d1076a13743edfec2611a658785826
Devon Meunier <devon.meunier@shopify.com>
2021-02-08 08:42:31 -0500

    Local plug.vim

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,8 @@
 sudo apt-get install -y nodejs ripgrep neovim fzf
 
-ln -f -s ~/dotfiles/.config/nvim ~/.config
+ln -f -s ~/dotfiles/.config/nvim ~/.config/nvim 
 ln -f -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -f -s ~/dotfiles/.zshrc ~/.zshrc
-
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-		https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 nvim --headless +PlugInstall +qa
 tmux source ~/.tmux.conf
