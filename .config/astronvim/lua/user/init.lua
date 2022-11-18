9de5b77b611c1072fbf44fd3ac9b9b10c499d892
Devon Meunier <devon.meunier@shopify.com>
2022-11-18 10:36:35 -0500

    Add rubocop

--- .config/astronvim/lua/user/init.lua
+++ .config/astronvim/lua/user/init.lua
@@ -1,15 +1,32 @@
-vim.g['test#strategy'] = 'toggleterm'
-
 return {
   lsp = {
     servers = {
       "sorbet"
+    },
+    formatting = {
+      format_on_save = true,
+      async = true,
     }
   },
   plugins = {
     init = {
       { "tpope/vim-endwise" },
       { "vim-test/vim-test" }
-    }
-  }
+    },
+    ["null-ls"] = function(config)
+      local null_ls = require "null-ls"
+      config.debug = true
+      config.sources = {
+        null_ls.builtins.formatting.rubocop.with({
+          command = "bin/rubocop",
+          timeout = 20000,
+          async = true,
+        }),
+      }
+      return config
+    end,
+  },
+  polish = function()
+    vim.g['test#strategy'] = 'toggleterm'
+  end
 }
