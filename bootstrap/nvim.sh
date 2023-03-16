b72884f2dbde8a3b0379def6a7f848285d18aa96
Devon Meunier <devon.meunier@shopify.com>
2023-03-16 13:54:11 -0400

    Don't

--- bootstrap/nvim.sh
+++ bootstrap/nvim.sh
@@ -2,6 +2,3 @@
 
 ln -f -s ~/dotfiles/.config/astronvim ~/.config/astronvim
 git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/nvim
-
-nvim  --headless -c 'autocmd User LazyDone quitall'
-
