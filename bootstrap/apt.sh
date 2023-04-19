84137083bd4783f747be550a49c6dd11880856d3
Devon Meunier <devon.meunier@shopify.com>
2023-04-19 14:52:40 -0400

    Add eternal terminal / tmux -CC setup

--- bootstrap/apt.sh
+++ bootstrap/apt.sh
@@ -1,9 +1,13 @@
+sudo add-apt-repository ppa:jgmath2000/et --yes
 sudo apt-get update
 sudo apt-get install -y \
+	et \
 	fzf \
 	mycli \
 	nodejs \
 	ripgrep \
 	zsh-syntax-highlighting \
 	exuberant-ctags
+systemctl enable et
+systemctl start et
 
